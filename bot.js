a2b8f7519b86d32652a5091763e96494e313299f
Unknown <dev.mohamed.amr@outlook.com>
2019-01-21 21:53:28 +0200

    selfbot fix / restructure / switch for commands

--- bot.js
+++ bot.js
@@ -1,15 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const SERVER_PREFIX = "$"
+
 // Trigger New Message Sent
 client.on('message', msg => {
+    //Restructure the message
+    const message = {   prefix: msg.content[0] , 
+                        command: msg.content.toLowerCase().slice(1).split(" ")[0],
+                        args: msg.content.toLowerCase().split(" ").slice(1),
+                    }
 
-    // reply with pong when read the message contain ping
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+    //Check if Self Talking
+    if(msg.author.id == client.id ||
+        message.prefix != SERVER_PREFIX
+        ) return;
+
+    //switch between commands
+    switch (message.command) {
+        case "ping":
+            msg.reply("pong");
+            break;
+        case "test":
+            msg.reply("Testing Success");
+            break;
+        default:
+            msg.reply("Not A Registered Command");
+            break;
     }
-
+    console.log(message);
 });
+
 
 // Login Identity
 client.login(process.env.BOT_IDENTITY);
